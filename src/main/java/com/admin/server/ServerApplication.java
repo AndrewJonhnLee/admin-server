fa23e5915b51306e4cec63bc8dd91eff36fad206
lee <andrew@sina.com>
2018-02-08 11:51:53 +0800

    使用springApplicationBuilder关闭命令传递参数

--- src/main/java/com/admin/server/ServerApplication.java
+++ src/main/java/com/admin/server/ServerApplication.java
@@ -11,6 +11,20 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+
+		SpringApplication app = new SpringApplication(ServerApplication.class);
+		app.setAddCommandLineProperties(false);
+		app.run(args);
+
+//		new SpringApplicationBuilder()
+//				.sources(Parent.class)
+//				.child(Application.class)
+//				.bannerMode(Banner.Mode.OFF)
+//				.run(args);
+
+//		ConfigurableApplicationContext context= SpringApplication.run(ClientApplication.class, args);
+//
+//		KafkaSender sender = context.getBean(KafkaSender.class);
+
 	}
 }
